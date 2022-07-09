e28f705af9eb62e8f7689e23eb9a000d2ead04af
Youngseo Choi <ys550499@gmail.com>
2022-07-09 10:06:07 +0900

    강의 ~ #2.5, Navigation, 로그아웃

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,30 @@
 import React from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import Navigation from "components/Navigation";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
 
 const AppRouter = ({isLoggedIn}) => {
   return <Router>
+    {isLoggedIn && <Navigation/>}
     <Switch>
       {isLoggedIn ?
           <>
             <Route exact path="/">
               <Home/>
             </Route>
-          </> :
-          <Route exact path="/"><Auth/></Route>
+            <Route exact path="/profile">
+              <Profile/>
+            </Route>
+          </>
+          :
+          <>
+            <Route exact path="/">
+              <Auth/>
+            </Route>
+            <Redirect from="*" to="/"/>
+          </>
       }
     </Switch>
   </Router>

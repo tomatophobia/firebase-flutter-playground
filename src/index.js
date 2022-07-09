e794e79bae94bcc81ddd412883ab1b240e8c92e8
Youngseo Choi <ys550499@gmail.com>
2022-07-09 13:06:44 +0900

    강의 ~ #3.2, create, read 구현

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import App from 'components/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // https://velog.io/@kysung95/짤막글-react-strict-모드란
+  // strict mode를 사용하는 경우 2번 호출되는 함수들이 생긴다
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
 )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { dbService } from 'fbase'
-import { collection, addDoc } from 'firebase/firestore'
+import { collection, addDoc, getDocs } from 'firebase/firestore'
 
 const Home = () => {
   const [nweet, setNweet] = useState('')
   const [nweets, setNweets] = useState([])
+
+  const getNweets = async () => {
+    const dbNweets = await getDocs(collection(dbService, 'nweets'))
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      }
+      setNweets((prev) => [nweetObject, ...prev])
+    })
+  }
+
+  useEffect(() => {
+    getNweets()
+  }, [])
+
   const onSubmit = async (event) => {
     event.preventDefault()
     await addDoc(collection(dbService, 'nweets'), {
@@ -19,6 +35,7 @@ const Home = () => {
     } = event
     setNweet(value)
   }
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -31,6 +48,13 @@ const Home = () => {
         />
         <input type="submit" value="nweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }

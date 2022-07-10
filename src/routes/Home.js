657f7b110931c4e9b352df397ee2f514a01a5772
Youngseo Choi <ys550499@gmail.com>
2022-07-10 16:48:42 +0900

    강의 ~ #4.1, 이미지 등록 틀 넣기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,6 +6,7 @@ import Nweet from 'components/Nweet'
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState('')
   const [nweets, setNweets] = useState([])
+  const [attachment, setAttachment] = useState(null)
 
   useEffect(() => {
     onSnapshot(collection(dbService, 'nweets'), (snapshot) => {
@@ -19,12 +20,13 @@ const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault()
-    await addDoc(collection(dbService, 'nweets'), {
-      text: nweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    })
-    setNweet('')
+    // await addDoc(collection(dbService, 'nweets'), {
+    //   text: nweet,
+    //   createdAt: Date.now(),
+    //   creatorId: userObj.uid,
+    // })
+    // setNweet('')
+
   }
   const onChange = (event) => {
     const {
@@ -32,6 +34,21 @@ const Home = ({ userObj }) => {
     } = event
     setNweet(value)
   }
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event
+    const theFile = files[0]
+    const reader = new FileReader()
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent
+      setAttachment(result)
+    }
+    reader.readAsDataURL(theFile)
+  }
+  const onClearAttachmentClick = () => setAttachment(null)
 
   return (
     <div>
@@ -43,7 +60,14 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="nweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachmentClick}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
